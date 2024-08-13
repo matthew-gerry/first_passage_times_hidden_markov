3cef1b9268c790df7e128d6f9edcdcf2b0e49509
Matthew Gerry <mgerry04@gmail.com>
2024-08-13 14:36:33 -0400

    Improvements to fitting functions, debugging, addition of the analysis of a chain

--- _utils.py
+++ _utils.py
@@ -9,8 +9,8 @@ import numpy as np
 from scipy.linalg import expm
 from scipy.optimize import least_squares
 
-from tensorflow import Variable, GradientTape
-from tensorflow.math import pow, reduce_sum
+from tensorflow import Variable, GradientTape, cast, float32
+import tensorflow.math as tfmath
 import tensorflow.keras.optimizers as ko
 
 
@@ -23,10 +23,11 @@ def first_passage_time_dist(L, start, leak, k0, time):
     THE ARGUMENT L MUST BE A RATE MATRIX OF THE HIDDEN NETWORK WITH ALL COLUMNS SUMMING TO ZERO.
     '''
 
-    # Substract the leak rate k0 from the diagonal element associated with the leak site
-    L[leak, leak] -= k0
-
     N = np.shape(L)[0] # Size of the hidden network
+
+    # Substract the leak rate k0 from the diagonal element associated with the leak site
+    L_leak = np.zeros([N, N]); L_leak[leak, leak] = -k0
+    L_full = L + L_leak
 
     # Distributions associated with the system at the start and leak site, respectively
     p_start, p_leak = np.zeros(N), np.zeros(N)
@@ -36,7 +37,7 @@ def first_passage_time_dist(L, start, leak, k0, time):
     # Pre-allocate array to hold FPTD values
     FPTD = np.zeros(len(time))
     for i in range(len(time)):
-        surv = expm(L*time[i]) # Survivial matrix
+        surv = expm(L_full*time[i]) # Survivial matrix
 
         # FPTD representing -d/dt of survival probability given start and leak sites
         FPTD[i] = k0*p_leak.dot(surv.dot(p_start.transpose()))
@@ -61,11 +62,13 @@ def f_model(t, coefficients, decay_rates):
         return sum([x*np.exp(y*t) for x,y in zip(coefficients, decay_rates[:-1])]) + last_coeff*np.exp(decay_rates[-1]*t)
 
 
-def fit_exponentials(t, y, N, x0=None, gtol=None, num_guesses=1):
+def fit_exponentials(t, y, N, x0=None, gtol=None, num_guesses=1, weight_short=False):
     '''
     USE scipy_optimize.least_squares TO FIT DATA TO A SUM OF EXPONENTIAL TERMS
     
     INPUT AN INITIAL GUESS AND OPTIONALLY TRY A SPECIFIED NUMBER OF ADDITIONAL RANDOM INITIAL GUESSES AND SELECT THE BEST ONE
+
+    OPTIONALLY SCALE THE RESIDUALS BY AN EXPONENTIALLY DECAYING FUNCTION IN TIME TO EMPHASIZE THE SHORT TIME BEHAVIOUR
     '''
     
     # Set the initial guess to a list of ones if not specified
@@ -79,9 +82,16 @@ def fit_exponentials(t, y, N, x0=None, gtol=None, num_guesses=1):
         coefficients = theta[:N-1]
         decay_rates = theta[N-1:]
         ft = f_model(t, coefficients, decay_rates)
-        return ft - y
+
+        if weight_short: # Multiply by an exponentially decaying function to emphasize short-time behaviour in fitting
+            kernel = 50*np.exp(-t) + 1
+            output = np.multiply(ft - y, kernel)
+        else: 
+            output = ft - y
+
+        return output
     
-    result = least_squares(residuals, x0, gtol=gtol)
+    result = least_squares(residuals, x0, gtol=gtol, method='dogbox')
 
     # Carry out optimizaton additional times with randomized intial guesses
     if num_guesses > 1:
@@ -91,7 +101,7 @@ def fit_exponentials(t, y, N, x0=None, gtol=None, num_guesses=1):
             rand_guess = np.append(coeff_rand_guess, rate_rand_guess)
 
             # Carry out optimization with randomly guessed initial values
-            result_temp = least_squares(residuals, rand_guess, gtol=gtol)
+            result_temp = least_squares(residuals, rand_guess, gtol=gtol, method='dogbox')
 
             if result_temp["cost"] < result["cost"]:
                 result = result_temp # If the random guess minimizes the cost function more effectively, replace the old result
@@ -99,9 +109,11 @@ def fit_exponentials(t, y, N, x0=None, gtol=None, num_guesses=1):
     return result
 
 
-def fit_exponentials_adam(t, y, N, x0=None, num_steps=200, learning_rate=0.1, epsilon=1e-8):
+def fit_exponentials_adam(t, y, N, x0=None, num_steps=200, learning_rate=0.1, epsilon=1e-8, weight_short=False):
     '''
-    CARRY OUT OPTIMIZATION USING THE ADAM ALGORITHM TO FIT THE DISTRIBUTION TO A SUMMATION OF EXPONENTIALS
+    CARRY OUT OPTIMIZATION USING THE ADAM ALGORITHM TO FIT THE DISTRIBUTION TO A SUMMATION OF EXPONENTIALS.
+
+    INCLUDES OPTION TO WEIGHT SHORT TIME VALUES MORE HEAVILY
     '''
     
     # Set the initial guess to a list of ones if not specified
@@ -111,11 +123,21 @@ def fit_exponentials_adam(t, y, N, x0=None, num_steps=200, learning_rate=0.1, ep
         raise ValueError("Initial guess must include 2*N - 1 parameter values.")
     
     # Define the cost function - sum of square error
-    def sse(theta):
+    def cost(theta):
         coefficients = theta[:N-1]
         decay_rates = theta[N-1:]
         ft = f_model(t, coefficients, decay_rates)
-        return reduce_sum(pow(ft - y, 2))
+        res = ft - y # Residuals
+
+        if weight_short:
+            kernel = 50.0*tfmath.exp(-0.2*t) + 1.0
+            kernel = cast(kernel, float32)
+            weighted_res = tfmath.multiply(res, kernel)
+            output = tfmath.reduce_sum(tfmath.pow(weighted_res, 2))
+        else:
+            output = tfmath.reduce_sum(tfmath.pow(res, 2))
+        
+        return output
 
     # Initialize Adam optimizer
     optimizer = ko.Adam(learning_rate=learning_rate, epsilon=epsilon,)
@@ -126,17 +148,17 @@ def fit_exponentials_adam(t, y, N, x0=None, num_steps=200, learning_rate=0.1, ep
     for i in range(num_steps):
         with GradientTape() as tape:
             # Calculate the value of the cost function and its gradient, store values
-            cost_value = sse(theta)
+            cost_value = cost(theta)
             gradient = tape.gradient(cost_value, theta)
 
         #  Use the Adam optimizer to update the value of theta
         optimizer.apply_gradients([(gradient, theta)])
     
     x = list(theta.numpy()) # Optimized parameter values
-    cost = sse(list(theta.numpy())) # Cost function at the end of optimization
+    costval = cost(list(theta.numpy())) # Cost function at the end of optimization
 
     # Output the parameter values as the end of optimization as well as the cost function
-    return dict(zip(["x", "cost"], [x, cost]))
+    return dict(zip(["x", "cost"], [x, costval]))
 
 
 def eig_L(L, leak, k0):
@@ -144,13 +166,14 @@ def eig_L(L, leak, k0):
     EIGENVALUES AND NORMALIZED EIGENVECTORS OF A GIVEN RATE MATRIX INCLUDING THE LEAK
     '''
 
-    # Substract the leak rate k0 from the diagonal element associated with the leak site
-    L[leak, leak] -= k0
-
     N = np.shape(L)[0] # Size of the hidden network
 
+    # Substract the leak rate k0 from the diagonal element associated with the leak site
+    L_leak = np.zeros([N, N]); L_leak[leak, leak] = -k0
+    L_full = L + L_leak
+
     # Numpy function to calculate eigenvalues and eigenvectors
-    eigvals, eigvecs = np.linalg.eig(L)
+    eigvals, eigvecs = np.linalg.eig(L_full)
 
     # Create a matrix whose columns will hold the normalized eigenvectors (elements sum to unity)
     V = np.zeros([N,N])
