3d29acadc72e7adbecefd44105b90c9663bc0ef1
Matthew Gerry <mgerry04@gmail.com>
2024-09-03 14:49:34 -0400

    Added calculations for a chain with an impurity

--- chain_fpt_stats.py
+++ chain_fpt_stats.py
@@ -13,7 +13,7 @@ from _utils import *
 import rate_matrices as rm
 
 # Set up time array
-dt = 0.03; tmax = 150
+dt = 0.03; tmax = 30
 time = np.arange(0, tmax, dt)
 
 # We will analyze how FPT statistics vary with N so set a range of chain lengths to analyze
@@ -21,15 +21,17 @@ Nrange = range(2, 7)
 k = 1 # Rate for site-site transitions in the uniform chain
 kA = 2/3; kB = 2 # Rates in the two different domains of the modular chain
 m = 1 # Segment length parameter for modular chains
-b = 0 # bias
+b = 10 # bias
 # k0 = 1 # Leak rate
 
 FPTD_N_uni = np.zeros([len(Nrange), len(time)]) # Array to store full FPTDs at every N for uniform chains
 FPTD_N_mod = np.zeros([len(Nrange), len(time)]) # Array to store full FPTDs at every N for modular chains
+FPTD_N_imp = np.zeros([len(Nrange), len(time)]) # Array to store full FPTDs at every N for impurity chains
 
  # Arrays to store first and second moments of FPTD at each N for each chain type
-M1_N_uni, M2_N_uni = np.zeros(len(Nrange)), np.zeros(len(Nrange))
-M1_N_mod, M2_N_mod = np.zeros(len(Nrange)), np.zeros(len(Nrange))
+M1_N_uni, M2_N_uni, M3_N_uni = np.zeros(len(Nrange)), np.zeros(len(Nrange)), np.zeros(len(Nrange))
+M1_N_mod, M2_N_mod, M3_N_mod = np.zeros(len(Nrange)), np.zeros(len(Nrange)), np.zeros(len(Nrange))
+M1_N_imp, M2_N_imp, M3_N_imp = np.zeros(len(Nrange)), np.zeros(len(Nrange)), np.zeros(len(Nrange))
 
 # Calculations for uniform chains
 for i in range(len(Nrange)):
@@ -45,6 +47,7 @@ for i in range(len(Nrange)):
     # Save the first two moments
     M1_N_uni[i] = fpt_moments(time, FPTD, 1)
     M2_N_uni[i] = fpt_moments(time, FPTD, 2)
+    M3_N_uni[i] = fpt_moments(time, FPTD, 3)
 
 
 # Calculations for modular chains
@@ -65,20 +68,43 @@ for i in range(len(Nrange)):
     # Save the first two moments
     M1_N_mod[i] = fpt_moments(time, FPTD, 1)
     M2_N_mod[i] = fpt_moments(time, FPTD, 2)
+    M3_N_mod[i] = fpt_moments(time, FPTD, 3)
+
+
+# Calculations for chains with an impurity
+for i in range(len(Nrange)):
+    N = Nrange[i]
+
+    L = rm.L_impurity(N, 0, k, 0.8*k, b) # Impurity placed at site 0 so it works for all site lengths
+    start_site = 0; leak_site = N - 1 # Choice of start and leak sites
+
+    # Calculate the exact FPTD for this rate matrix and choice of start/leak sites    
+    FPTD = first_passage_time_dist(L, start_site, leak_site, k, time)
+    FPTD_N_imp[i, :] = FPTD
+
+    # Save the first two moments
+    M1_N_imp[i] = fpt_moments(time, FPTD, 1)
+    M2_N_imp[i] = fpt_moments(time, FPTD, 2)
+    M3_N_imp[i] = fpt_moments(time, FPTD, 3)
 
 
 # Calculate randomness parameter
 r_uni = (M2_N_uni - M1_N_uni**2)/M1_N_uni**2
 r_mod = (M2_N_mod - M1_N_mod**2)/M1_N_mod**2
+r_imp = (M2_N_imp - M1_N_imp**2)/M1_N_imp**2
+
 
 # Plot the first passage time distributions at each N as a function of time for each chain type
-plt.subplot(1,2,1)
+plt.subplot(1,3,1)
 for i in range(len(Nrange)):
     plt.plot(time, FPTD_N_uni[i, :], '-', label = "$N = $" + str(Nrange[i]))
 plt.legend()
-plt.subplot(1,2,2)
+plt.subplot(1,3,2)
 for i in range(len(Nrange)):
     plt.plot(time, FPTD_N_mod[i, :], '-', label = "$N = $" + str(Nrange[i]))
+plt.subplot(1,3,3)
+for i in range(len(Nrange)):
+    plt.plot(time, FPTD_N_imp[i, :], '-', label = "$N = $" + str(Nrange[i]))
 plt.show()
 
 # Show a scatter plot of the first two moments
@@ -87,7 +113,21 @@ ax.scatter(M1_N_uni, r_uni, label="Uniform")
 for i, N in enumerate(Nrange):
     ax.annotate("N = "+str(N), (M1_N_uni[i], r_uni[i]))
 ax.scatter(M1_N_mod, r_mod, label="Modular")
+ax.scatter(M1_N_imp, r_imp, label="Impurity")
 ax.set_xlabel(r"$\langle t\rangle$")
 # ax.set_ylabel(r"$\langle t^2\rangle$")
 ax.set_ylabel("$r$")
+plt.legend()
+plt.show()
+
+fig, ax = plt.subplots()
+ax.scatter(Nrange, M3_N_uni, label="Uniform")
+for i, N in enumerate(Nrange):
+    ax.annotate("N = "+str(N), (M1_N_uni[i], r_uni[i]))
+ax.scatter(Nrange, M3_N_mod, label="Modular")
+ax.scatter(Nrange, M3_N_imp, label="Impurity")
+ax.set_xlabel(r"$N$")
+# ax.set_ylabel(r"$\langle t^2\rangle$")
+ax.set_ylabel(r"$\langle t^3\rangle$")
+plt.legend()
 plt.show()
