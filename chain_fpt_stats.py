0b4b04e2c8fbd91ecad8ffe2d8da6e26bc63ad90
Matthew Gerry <mgerry04@gmail.com>
2024-09-03 16:40:04 -0400

    plotting third cumulant instead of third moment

--- chain_fpt_stats.py
+++ chain_fpt_stats.py
@@ -13,7 +13,7 @@ from _utils import *
 import rate_matrices as rm
 
 # Set up time array
-dt = 0.03; tmax = 30
+dt = 0.03; tmax = 150
 time = np.arange(0, tmax, dt)
 
 # We will analyze how FPT statistics vary with N so set a range of chain lengths to analyze
@@ -21,7 +21,7 @@ Nrange = range(2, 7)
 k = 1 # Rate for site-site transitions in the uniform chain
 kA = 2/3; kB = 2 # Rates in the two different domains of the modular chain
 m = 1 # Segment length parameter for modular chains
-b = 10 # bias
+b = 0 # bias
 # k0 = 1 # Leak rate
 
 FPTD_N_uni = np.zeros([len(Nrange), len(time)]) # Array to store full FPTDs at every N for uniform chains
@@ -75,7 +75,7 @@ for i in range(len(Nrange)):
 for i in range(len(Nrange)):
     N = Nrange[i]
 
-    L = rm.L_impurity(N, 0, k, 0.8*k, b) # Impurity placed at site 0 so it works for all site lengths
+    L = rm.L_impurity(N, 0, k, 0.5*k, b) # Impurity placed at site 0 so it works for all site lengths
     start_site = 0; leak_site = N - 1 # Choice of start and leak sites
 
     # Calculate the exact FPTD for this rate matrix and choice of start/leak sites    
@@ -92,6 +92,10 @@ for i in range(len(Nrange)):
 r_uni = (M2_N_uni - M1_N_uni**2)/M1_N_uni**2
 r_mod = (M2_N_mod - M1_N_mod**2)/M1_N_mod**2
 r_imp = (M2_N_imp - M1_N_imp**2)/M1_N_imp**2
+
+sk_uni = (M3_N_uni - M1_N_uni**3)/M1_N_uni**3
+sk_mod = (M3_N_mod - M1_N_mod**3)/M1_N_mod**3
+sk_imp = (M3_N_imp - M1_N_imp**3)/M1_N_imp**3
 
 
 # Plot the first passage time distributions at each N as a function of time for each chain type
@@ -121,13 +125,12 @@ plt.legend()
 plt.show()
 
 fig, ax = plt.subplots()
-ax.scatter(Nrange, M3_N_uni, label="Uniform")
+ax.scatter(M1_N_uni, sk_uni, label="Uniform")
 for i, N in enumerate(Nrange):
-    ax.annotate("N = "+str(N), (M1_N_uni[i], r_uni[i]))
-ax.scatter(Nrange, M3_N_mod, label="Modular")
-ax.scatter(Nrange, M3_N_imp, label="Impurity")
-ax.set_xlabel(r"$N$")
-# ax.set_ylabel(r"$\langle t^2\rangle$")
-ax.set_ylabel(r"$\langle t^3\rangle$")
+    ax.annotate("N = "+str(N), (M1_N_uni[i], sk_uni[i]))
+ax.scatter(M1_N_mod, sk_mod, label="Modular")
+ax.scatter(M1_N_imp, sk_imp, label="Impurity")
+ax.set_xlabel(r"$\langle t\rangle$")
+ax.set_ylabel(r"$\langle\langle t^3\rangle\rangle$")
 plt.legend()
 plt.show()
